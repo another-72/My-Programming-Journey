3078536959398b7fc086433e00b577904dfd719a
another-72 <1010.mr.mridul@gmail.com>
2026-02-05 20:05:11 +0530

    Add files via upload

---
+++ act.cpp
@@ -0,0 +1,55 @@
+/*
+    Date : 5th Feb 2026
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+ll rnd(ll l, ll r)
+{
+   return uniform_int_distribution<ll>(l, r)(rng);
+}
+
+template <typename T>
+using ordered_multiset = tree<
+    T,
+    null_type,
+    less_equal<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+bool MULTI_TEST = true;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    if(n & 1) cout << 0 << '\n';
+    else cout << (n / 4) + 1 << '\n';
+}
+
+int main()
+{
+    auto start = chrono::high_resolution_clock::now();
+    fastio();
+
+    int t = 1;
+    if(MULTI_TEST) cin >> t;
+
+    while(t--) solve();
+
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    cerr << "Runtime : " << elapsed.count() << " ms\n";
+    return 0;
+}
