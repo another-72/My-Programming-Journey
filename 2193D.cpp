ffbd2825f62aa8e628d2471d12680ebbb04459fe
another-72 <1010.mr.mridul@gmail.com>
2026-02-13 03:15:47 +0530

    Add initial implementation of problem 2193D

---
+++ 2193D.cpp
@@ -0,0 +1,72 @@
+/*
+    Date : 13th Feb 2026
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+ll rnd(ll l, ll r)
+{
+   return uniform_int_distribution<ll>(l, r)(rng);
+}
+
+template <typename T>
+using ordered_multiset = tree<
+    T,
+    null_type,
+    less_equal<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+bool MULTI_TEST = true;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    vector<ll> a(n, 0), b(n, 0);
+
+    for(ll i = 0; i < n; i++) cin >> a[i];
+    for(ll i = 0; i < n; i++) cin >> b[i];
+
+    sort(a.begin(), a.end());
+
+    vector<ll> pf(n, b[0]);
+    for(ll i = 1; i < n; i++) pf[i] = pf[i - 1] + b[i];
+
+    ll ans = 0;
+    for(ll i = 0; i < n; i++)
+    {
+        ll greater_elements = n - i;
+        ll level = upper_bound(pf.begin(), pf.end(), greater_elements) - pf.begin();
+        ans = max(ans, level * a[i]);
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    auto start = chrono::high_resolution_clock::now();
+    fastio();
+
+    int t = 1;
+    if(MULTI_TEST) cin >> t;
+
+    while(t--) solve();
+
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    cerr << "Runtime : " << elapsed.count() << " ms\n";
+    return 0;
+}
